b2e265120311e5d4d96b5ad5a90313a7f0956b24
錢承 <cc@J-MacBook.local>
2023-09-30 00:15:50 +0800

    HW2 finish

--- hw2/311551096.js
+++ hw2/311551096.js
@@ -1,16 +1,6 @@
-
-// set the dimensions and margins of the graph
 const margin = {top: 20, right: 30, bottom: 80, left: 30},
   width = 910 - margin.left - margin.right,
-  height = 950 - margin.top - margin.bottom;
-// append the svg object to the body of the page
-const svg = d3.select("#container")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        `translate(${margin.left},${margin.top})`);
+  height = 1200 - margin.top - margin.bottom;
 
 const dropdownMenu = (selection, props) => {
     const {
@@ -18,6 +8,7 @@ const dropdownMenu = (selection, props) => {
         onOptionClicked,
         selectedOption
     } = props;
+    
     
     let select = selection.selectAll('select').data([null]);
     select = select.enter().append('select')
@@ -35,26 +26,34 @@ const dropdownMenu = (selection, props) => {
         .text(d => d)
     };
 
+
     let DATA;
     let yColumn1, yColumn2, yColumn3, yColumn4;
-    let dimensions = ["sepal length", "sepal width", "petal length", "petal width"];
+    let dimensions;
     const onYColumnClicked1 = column => {
+    dimensions[0] = column;
     yColumn1 = column;
-    render();
+    d3.select("svg").remove();
+    render(dimensions);
     };
     const onYColumnClicked2 = column => {
+    dimensions[1] = column;
     yColumn2 = column;
-    render();
+    d3.select("svg").remove();
+    render(dimensions);
     };
     const onYColumnClicked3 = column => {
+    dimensions[2] = column;
     yColumn3 = column;
-    render();
+    d3.select("svg").remove();
+    render(dimensions);
     };
     const onYColumnClicked4 = column => {
+    dimensions[3] = column;
     yColumn4 = column;
-    render();
-    };
-
+    d3.select("svg").remove();
+    render(dimensions);
+    };  
 
 
     const render = (dimensions) => {
@@ -101,35 +100,32 @@ const dropdownMenu = (selection, props) => {
         
         const color = d3.scaleOrdinal()
         .domain(["Iris-setosa", "Iris-versicolor", "Iris-virginica" ])
-        .range([ "#440154ff", "#21908dff", "#fde725ff"])
+        .range([ "#ff7f0e", "#2ca02c", "#1f77b4"])
         
 
-        
-          // For each dimension, I build a linear scale. I store all in a y object
           const y = {}
           for (i in dimensions) {
             n = dimensions[i]
             y[n] = d3.scaleLinear()
               .domain( [0,8] )
-              .range([height, 0])
+              .range([height-480, 0])
           }
         
-          // Build the X scale -> it find the best position for each Y axis
+
+        params = dimensions.map(function(d, i) {
+            return d + "-" + i;
+          });
+          console.log('params:',params)
           x = d3.scalePoint()
             .range([0, width])
-            .domain(dimensions);
-        
-          // Highlight the specie that is hovered
+            .domain(params);
           const highlight = function(event, d){
         
             selected_specie = d.class
-        
-            // first every group turns grey
             d3.selectAll(".line")
               .transition().duration(200)
               .style("stroke", "lightgrey")
               .style("opacity", "0.2")
-            // Second the hovered specie takes its color
             d3.selectAll("." + selected_specie)
               .transition().duration(200)
               .style("stroke", color(selected_specie))
@@ -144,50 +140,80 @@ const dropdownMenu = (selection, props) => {
               .style("opacity", "1")
           }
         
-          // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+          //input: row of the csv
+          //output: x and y
           function path(d) {
-              return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+              return d3.line()(params.map(function(p) { return [x(p), y[p.substr(0, p.length-2)](d[p.substr(0, p.length-2)])]; }));
           }
         
           // Draw the lines
+          const svg = d3.select("#container")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform",
+                  `translate(${margin.left},${margin.top})`);
           svg
             .selectAll("myPath")
             .data(DATA)
             .join("path")
-              .attr("class", function (d) { return "line " + d.class } ) // 2 class for each line: 'line' and the group name
+              .attr("class", function (d) { return "line " + d.class } )
               .attr("d",  path)
               .style("fill", "none" )
               .style("stroke", function(d){ return( color(d.class))} )
-              .style("opacity", 0.5)
+              .style("opacity", 0.8)
               .on("mouseover", highlight)
               .on("mouseleave", doNotHighlight )
         
-          // Draw the axis:
-          svg.selectAll("myAxis")
-            // For each dimension of the dataset I add a 'g' element:
-            .data(dimensions).enter()
+          // Draw axis
+          svg
+            .selectAll("myAxis")
+            .data(params).enter()
             .append("g")
             .attr("class", "axis")
-            // I translate this element to its right position on the x axis
+            // I translate the element to right position on the x axis
             .attr("transform", function(d) { return `translate(${x(d)})`})
-            // And I build the axis with the call function
-            .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d])); })
-            // Add axis title
-            .append("text")
-              .style("text-anchor", "middle")
-              .attr("y", -9)
-              .text(function(d) { return d; })
-              .style("fill", "black")
+            // build the axis
+            .each(function(d) { d3.select(this).call(d3.axisLeft().ticks(5).scale(y[d.substr(0, d.length-2)])); })
+      
 
+             svg.append("text")
+             .attr("x", 5).attr("y", 640)
+             .text("Iris-setosa").style("font-size", "25px")
+             .attr("alignment-baseline","middle")
+             .style("fill", "#ff7f0e")
+     
+         svg.append("text")
+             .attr("x", 5).attr("y", 670)
+             .text("Iris-versicolor").style("font-size", "25px")
+             .attr("alignment-baseline","middle")
+             .style("fill", "#2ca02c")
+     
+         svg.append("text")
+             .attr("x", 5).attr("y", 700)
+             .text("Iris-virginica").style("font-size", "25px")
+             .attr("alignment-baseline","middle")
+             .style("fill", "#1f77b4")
+     
 
     };
 
-// Parse the Data
+function removeNaN(data,length){
+    for (var i = 0; i < length; i++) {
+        if( (!data[i]['sepal length']) || isNaN(data[i]['sepal length']) || isNaN(data[i]['sepal width']) || isNaN(data[i]['petal length']) || isNaN(data[i]['petal width']))
+        {
+        delete data[i];
+        data.length -= 1;
+        }
+    }
+    return data;
+}
+
 d3.csv("iris.csv").then( function(data) {
-  // Color scale: give me a specie name, I return a color
     dimensions = ["sepal length", "sepal width", "petal length", "petal width"];
-    console.log('First',dimensions)
-    DATA = data
+    console.log('First',dimensions);
+    DATA = removeNaN(data, data.length);
     render(dimensions);
 
 })
