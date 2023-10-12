143d47dac5bfef299d5ce230839dd34fd3544f2d
錢承 <cc@J-MacBook.local>
2023-10-12 17:46:19 +0800

    HW4 start

---
+++ hw4/311551096.js
@@ -0,0 +1,163 @@
+
+var width = 960,
+    size = 230,
+    padding = 20;
+
+var x = d3.scale.linear()
+    .range([padding / 2, size - padding / 2]);
+
+var y = d3.scale.linear()
+    .range([size - padding / 2, padding / 2]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .ticks(6);
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(6);
+
+function color(c){
+    s = ["Iris-setosa", "Iris-versicolor", "Iris-virginica" ];
+    colors = [ "#ff7f0e", "#2ca02c", "#1f77b4"];
+    return colors[s.indexOf(c)];
+}
+
+function cross(a, b) {
+    var c = [], n = a.length, m = b.length, i, j;
+    for (i = -1; ++i < n;) 
+        for (j = -1; ++j < m;) 
+            c.push({x: a[i], i: i, y: b[j], j: j});
+    return c;
+}
+
+function removeNaN(data,length){
+    for (var i = 0; i < length; i++) {
+        if( (!data[i]['sepal length']) || isNaN(data[i]['sepal length']) || isNaN(data[i]['sepal width']) || isNaN(data[i]['petal length']) || isNaN(data[i]['petal width']))
+        {
+        delete data[i];
+        data.length -= 1;
+        }
+    }
+    return data;
+}
+
+d3.csv("iris.csv", function(error, data) {
+    if (error) throw error;
+
+    data = removeNaN(data, data.length);
+
+    var domainByTrait = {};
+    var traits = d3.keys(data[0]).filter(function(d) { return d !== "class"; });
+    var n = traits.length;
+
+    traits.forEach(function(trait) {
+          domainByTrait[trait] = d3.extent(data, function(d) { return d[trait]; });
+        });
+        
+    console.log(traits)
+    // ["sepal length", "sepal width", "petal length", "petal width"]
+    
+
+    xAxis.tickSize(size * n);
+    yAxis.tickSize(-size * n);
+
+    var brush = d3.svg.brush()
+        .x(x)
+        .y(y)
+        .on("brushstart", brushstart)
+        .on("brush", brushmove)
+        .on("brushend", brushend);
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", size * n + padding)
+        .attr("height", size * n + padding)
+        .append("g")
+        .attr("transform", "translate(" + padding + "," + padding / 2 + ")");
+
+    svg.selectAll(".x.axis")
+        .data(traits)
+        .enter().append("g")
+        .attr("class", "x axis")
+        .attr("transform", function(d, i) { return "translate(" + (n - i - 1) * size + ",0)"; })
+        .each(function(d) { x.domain(domainByTrait[d]); d3.select(this).call(xAxis); });
+
+    svg.selectAll(".y.axis")
+        .data(traits)
+        .enter().append("g")
+        .attr("class", "y axis")
+        .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
+        .each(function(d) { y.domain(domainByTrait[d]); d3.select(this).call(yAxis); });
+
+    var cell = svg.selectAll(".cell")
+        .data(cross(traits, traits))
+        .enter().append("g")
+        .attr("class", "cell")
+        .attr("transform", function(d) { return "translate(" + (n - d.i - 1) * size + "," + d.j * size + ")"; })
+        .each(plot);
+
+    // Titles for the diagonal.
+    cell.filter(function(d) { return d.i === d.j; }).append("text")
+        .attr("x", padding)
+        .attr("y", padding)
+        .attr("dy", ".71em")
+        .text(function(d) { return d.x; });
+
+    cell.call(brush);
+
+    function plot(p) {
+        console.log("p",p)
+        var cell = d3.select(this);
+
+        x.domain(domainByTrait[p.x]);
+        y.domain(domainByTrait[p.y]);
+
+        if(p.x == p.y){
+            return;
+        }
+        else{
+            cell.append("rect")
+                .attr("class", "frame")
+                .attr("x", padding / 2)
+                .attr("y", padding / 2)
+                .attr("width", size - padding)
+                .attr("height", size - padding);
+
+            cell.selectAll("circle")
+                .data(data)
+            .enter().append("circle")
+                .attr("cx", function(d) { return x(d[p.x]); })
+                .attr("cy", function(d) { return y(d[p.y]); })
+                .attr("r", 4)
+                .style("fill", function(d) { return color(d.class); });
+        }
+    }
+
+    var brushCell;
+
+    // Clear the previously-active brush, if any.
+    function brushstart(p) {
+        if (brushCell !== this) {
+            d3.select(brushCell).call(brush.clear());
+            x.domain(domainByTrait[p.x]);
+            y.domain(domainByTrait[p.y]);
+            brushCell = this;
+        }
+    }
+
+    // Highlight the selected circles.
+    function brushmove(p) {
+        var e = brush.extent();
+        svg.selectAll("circle").classed("hidden", function(d) {
+        return e[0][0] > d[p.x] || d[p.x] > e[1][0]
+            || e[0][1] > d[p.y] || d[p.y] > e[1][1];
+        });
+    }
+
+    // If the brush is empty, select all circles.
+    function brushend() {
+        if (brush.empty()) svg.selectAll(".hidden").classed("hidden", false);
+    }
+});
