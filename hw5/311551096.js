1c1cb44afce3efed3c31d3b953805d57f8678516
Aki Lin <akilin@AkideMacBook-Air.local>
2023-10-22 18:09:23 +0800

    hw5

--- hw5/311551096.js
+++ hw5/311551096.js
@@ -1,14 +1,15 @@
 
 // Constants
-const margin = { top: 20, right: 20, bottom: 30, left: 200 },
+const margin = { top: 20, right: 20, bottom: 30, left: 200 }, //絕對位置
     width = 960 - margin.left - margin.right,
-    height = 18799 - margin.top - margin.bottom; // Adjusted height
+    height = 20000 - margin.top - margin.bottom; // Adjusted height
     // 20000
 
 // Scales and Axes
 const x = d3.scaleLinear().range([0, width]);
-const y = d3.scaleBand().rangeRound([0, height], 0.03)
-                        .paddingInner(0.08); // Adjusted y scale
+const y = d3.scaleBand().rangeRound([0, 30000], 0.1) //調整scale大小
+                        .paddingInner(0.2)// Adjusted y scale
+                        .paddingOuter(0.8);
 const color = d3.scaleOrdinal(d3.schemeCategory10);
 
 const xAxis = d3.axisTop(x);
@@ -18,8 +19,8 @@ let allData;
 
 // SVG canvas
 const svg = d3.select(".chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right+100)
+    .attr("height", 21300)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -50,6 +51,7 @@ function updateChart(data) {
         d.total = d.criteria[d.criteria.length - 1].y1;
     });
 
+
     y.domain(data.map(d => d.name));
     x.domain([0, d3.max(data, d => d.total)]);
 
@@ -59,12 +61,14 @@ function updateChart(data) {
 
     svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(0," + -300  + ")") // This moves the x-axis to the bottom of the SVG
         .call(yAxis)
         .selectAll("text")
         .style("text-anchor", "end")
         .attr("dx", "-.5em")
         .attr("dy", "-.5em")
         .attr("transform", "rotate(-35)");
+        
 
     const university = svg.selectAll(".university")
         .data(data)
@@ -74,21 +78,24 @@ function updateChart(data) {
 
     university.selectAll("rect")
         .data(d => d["criteria"].map(obj => {
+            obj['scores_overall'] = d['scores_overall'];
             obj['university_name'] = d['name'];
             obj['criteria'] = d['criteria'];
             return obj;
           }))
         .enter().append("rect")
+        .attr("transform", "translate(0," + -300  + ")")
         .attr("height", y.bandwidth())
+        .attr("width", 100)
         .attr("x", d => x(d.y0))
-        .attr("width", d => x(d.y1) - x(d.y0))
+        .attr("width", d => (x(d.y1) - x(d.y0)))
         .style("fill", d => color(d.name))
         .on("mouseover", function(event, d) {
-            // console.log("d:",d.criteria);
             tooltip.transition()        
                 .duration(200)      
                 .style("opacity", 1);    
             tooltip.html(d.university_name + '<br/>'
+                        + "Overall: " + d.scores_overall  + '<br/>'
                         +d.criteria[0].name.replace("scores_","").replace("_"," ") + ": " + Math.round((d.criteria[0].y1 - d.criteria[0].y0)*1000)/1000 + '<br/>'
                         +d.criteria[1].name.replace("scores_","").replace("_"," ") + ": " + Math.round((d.criteria[1].y1 - d.criteria[1].y0)*1000)/1000 + '<br/>'
                         +d.criteria[2].name.replace("scores_","").replace("_"," ") + ": " + Math.round((d.criteria[2].y1 - d.criteria[2].y0)*1000)/1000 + '<br/>'
@@ -106,33 +113,43 @@ function updateChart(data) {
         });
 
         
-        
+    console.log("color.domain().slice().reverse():",color.domain().slice().reverse())
+
 
     const legend = svg.selectAll(".legend")
         .data(color.domain().slice().reverse())
         .enter().append("g")
         .attr("class", "legend")
-        .attr("transform", (d, i) => "translate(0," + i * 20 + ")");
+        .attr("transform", (d, i) => "translate(0," + (i * 20 + 20)+ ")");
 
     legend.append("rect")
-        .attr("x", width - 18)
-        .attr("width", 18)
-        .attr("height", 18)
+        .attr("x", width )
+        .attr("width", 25)
+        .attr("height", 20)
         .style("fill", color);
 
     legend.append("text")
-        .attr("x", width - 24)
+        .attr("x", width - 10)
         .attr("y", 9)
         .attr("dy", ".35em")
         .style("text-anchor", "end")
-        .text(d => d.replace("scores_","").replace("_"," "));
+        .text(d => d.replace("scores_","").replace("_"," "))
+        .each(function(d) {
+            var bbox = this.getBBox();
+            legend.insert("rect", "text")
+               .style("fill", "#FFE6F0")
+               .attr("x", bbox.x)
+               .attr("y", bbox.y)
+               .attr("width", bbox.width)
+               .attr("height", bbox.height);
+         });
+     ;
 }
 
 // Load data
 d3.csv("TIMES_WorldUniversityRankings_2024.csv").then(data => {
     allData = data;
     allData.forEach(d => {
-        d["scores_overall"]                 = d["scores_overall"]=='n/a'               ? 0 : +d["scores_overall"];
         d["scores_teaching"]                = d["scores_teaching"]=='n/a'              ? 0 : +d["scores_teaching"];
         d["scores_research"]                = d["scores_research"]=='n/a'              ? 0 : +d["scores_research"];
         d["scores_citations"]               = d["scores_citations"]=='n/a'             ? 0 : +d["scores_citations"];
